2c2f6953455326105be854d037af393daae44a64
Mark Ravinet <markravinet@Marks-MacBook-Pro-5.local>
2018-03-05 16:51:25 +0100

    upload pca covariation script

---
+++ denovo/plot_pc_covariation.R
@@ -0,0 +1,32 @@
+## look at covariation in PCs and data issues
+
+rm(list = ls())
+
+library(tidyverse)
+
+# read in pca
+pca <- read_csv("./pca_data.csv")
+
+# read in het
+het <- read_tsv("./cichlid.het", col_names = c("id", "ho", "he", "nsites", "f"), skip = 1)
+# read in depth
+depth <- read_tsv("./cichlid.idepth", col_names = c("id", "n", "mean_depth"), skip = 1)
+# read in missing data
+miss <- read_tsv("./cichlid.imiss", col_names = c("id", "n", "n_fil", "n_miss", "f_miss"), skip = 1)
+
+# join to make a dataset
+myData <- left_join(pca, het, by = "id")
+myData <- left_join(myData, depth, by = "id")
+myData <- left_join(myData, miss, by = "id")
+
+# subset data
+myData <- myData %>% select(-starts_with("n"))
+# gather data
+myData_g <- myData %>% select(-ho, -he) %>% gather(key = "stat", value = "value", -id, -pop, -PC1, -PC2)
+
+# plot data 
+a <- ggplot(myData_g, aes(value, PC1, colour = pop)) + geom_point() + facet_wrap(~stat, scales = "free_x")
+a + theme_light() + theme(legend.position = "bottom")
+
+dev.print(pdf, "./pca_corr.pdf", height = 6, width = 8)
+
